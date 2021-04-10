38ba41834083e36e21c42df2285c2c8df62df79d
Lucas-Arnould <lucas.arnould@outlook.com>
2021-04-10 10:00:16 +0200

    ajout d'un menu de navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,29 +2,11 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },
-  {
-    path: 'description-worksite',
-    loadChildren: () => import('./description-worksite/description-worksite.module').then( m => m.DescriptionWorksitePageModule)
-  },
-  {
-    path: 'add-worksite',
-    loadChildren: () => import('./add-worksite/add-worksite.module').then( m => m.AddWorksitePageModule)
-  },
-
-
-  {
-    path: 'list-worksite',
-    loadChildren: () => import('./list-worksite/list-worksite.module').then( m => m.ListWorksitePageModule)
-  },
+  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: '', redirectTo: 'home', pathMatch: 'full'},
+  { path: 'description-worksite', loadChildren: () => import('./description-worksite/description-worksite.module').then( m => m.DescriptionWorksitePageModule)},
+  { path: 'add-worksite', loadChildren: () => import('./add-worksite/add-worksite.module').then( m => m.AddWorksitePageModule)},
+  { path: 'list-worksite', loadChildren: () => import('./list-worksite/list-worksite.module').then( m => m.ListWorksitePageModule)},
 ];
 
 @NgModule({
